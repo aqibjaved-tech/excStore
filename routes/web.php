f7356b9af07a18d9367ba4c34109e4ad41379895
aqibjaved-tech <58594872+aqibjaved-tech@users.noreply.github.com>
2020-03-24 17:53:33 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 // Route::post('/create_site_folder','StoreViewController@sitepages');
 
 Route::group(['domain' => '{domain}'],  function () {
+            Route::get('/', 'Welcome');
 //          Route::get('/', ['uses'=>'StoreViewController@index', 'subdomain' => 'AQ']);
 //         Route::get('/brands', ['uses'=>'StoreViewController@brands','subdomain' => 'AQ']);
 
